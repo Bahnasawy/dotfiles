57551df00f2230b1b53676f413372df7b85778e4
Yousef Elbahnasawy <bahnasawy@Yousefs-MacBook-Pro.local>
2024-10-27 02:13:57 +0300

    Added GP Calendar

--- nix/mac/configs/homebrew.nix
+++ nix/mac/configs/homebrew.nix
@@ -22,6 +22,7 @@
     masApps = {
       "XCode" = 497799835;
       "Transporter" = 1450874784;
+      "GP Calendar" = 1632951751;
     };
     onActivation.cleanup = "zap";
     onActivation.autoUpdate = true;
