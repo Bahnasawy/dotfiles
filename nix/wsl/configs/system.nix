779800c22399d33e03404b1e72913aea223a4cbb
bahnasawy <yousef.elbahnasawy@gmail.com>
2024-12-25 11:53:41 +0000

    Fixed some linked libraries

--- nix/wsl/configs/system.nix
+++ nix/wsl/configs/system.nix
@@ -12,4 +12,14 @@
   };
 
   system.stateVersion = "25.05";
+
+  programs = {
+    nix-ld.enable = true;
+    nix-ld.libraries = with pkgs; [
+      lua-language-server
+      nodejs
+      # Add any missing dynamic libraries for unpackaged programs
+      # here, NOT in environment.systemPackages
+    ];
+  };
 }
