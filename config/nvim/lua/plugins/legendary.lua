380b99117f48902422adf4e8d7321e41625de8e8
bahnasawy <yousef.elbahnasawy@gmail.com>
2025-03-19 09:48:27 +0000

    WIP

--- config/nvim/lua/plugins/legendary.lua
+++ config/nvim/lua/plugins/legendary.lua
@@ -2,16 +2,17 @@ return {
   "mrjones2014/legendary.nvim",
   -- since legendary.nvim handles all your keymaps/commands,
   -- its recommended to load legendary.nvim before other plugins
-  priority = 10000,
+  priority = 1000000,
   lazy = false,
   -- sqlite is only needed if you want to use frecency sorting
   dependencies = { "kkharji/sqlite.lua" },
   opts = {
+    extensions = { lazy_nvim = true },
     keymaps = {
-      { "<Left>", { n = "<c-w>h" }, description = "Left Split", opts = { remap = true } },
-      { "<Right>", { n = "<c-w>l" }, description = "Right Split", opts = { remap = true } },
       { "<Up>", { n = "<c-w>k" }, description = "Upper Split", opts = { remap = true } },
       { "<Down>", { n = "<c-w>j" }, description = "Lower Split", opts = { remap = true } },
+      { "<Left>", { n = "<c-w>h" }, description = "Left Split", opts = { remap = true } },
+      { "<Right>", { n = "<c-w>l" }, description = "Right Split", opts = { remap = true } },
     },
   },
 }
