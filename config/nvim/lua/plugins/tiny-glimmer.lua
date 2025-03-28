edccdb720dfe7660db871e77872a09c9ea2e6557
Bahnasawy <yousef.elbahnasawy@gmail.com>
2025-03-28 16:57:49 +0000

    tiny-glimmer

--- config/nvim/lua/plugins/tiny-glimmer.lua
+++ config/nvim/lua/plugins/tiny-glimmer.lua
@@ -2,7 +2,26 @@ return {
   "rachartier/tiny-glimmer.nvim",
   event = "TextYankPost",
   lazy = false,
+  priority = 10,
   opts = {
-    -- your configuration
+    overwrite = {
+      yank = {
+        default_animation = "pulse",
+      },
+      undo = {
+        enable = true,
+      },
+      redo = {
+        enable = true,
+      },
+    },
+    presets = {
+      pulsar = {
+        enabled = true,
+        default_animation = {
+          name = "pulse",
+        },
+      },
+    },
   },
 }
