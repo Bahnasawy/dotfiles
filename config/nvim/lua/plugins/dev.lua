a833d1c02dd7263f633b71e794ffb159d739a85c
Yousef Elbahnasawy <yousef.elbahnasawy@gmail.com>
2025-08-11 02:42:34 +0300

    Updates and additions

--- config/nvim/lua/plugins/dev.lua
+++ config/nvim/lua/plugins/dev.lua
@@ -1,16 +1,4 @@
 return {
-  {
-    "mistweaverco/kulala.nvim",
-    keys = {
-      { "<leader>Rs", desc = "Send request" },
-      { "<leader>Ra", desc = "Send all requests" },
-      { "<leader>Rb", desc = "Open scratchpad" },
-    },
-    ft = { "http", "rest" },
-    opts = {
-      global_keymaps = true,
-    },
-  },
   -- Typescript
   { "dmmulroy/ts-error-translator.nvim", lazy = true, ft = { "typescriptreact", "typescript" } },
   {
