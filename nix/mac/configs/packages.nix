3c7af5ee893716c4a00271d614721d9d5a909e90
Yousef Elbahnasawy <yousef.elbahnasawy@gmail.com>
2026-09-15 11:33:23 +0300

    Herdr plugins and updates

--- nix/mac/configs/packages.nix
+++ nix/mac/configs/packages.nix
@@ -140,12 +140,6 @@
 
           eval "$(atuin init zsh)"
         '')
-
-        (lib.mkOrder 1100 ''
-          if [[ -z "$TMUX" ]] && command -v tmux >/dev/null 2>&1; then
-            exec tmux new-session -A -s main
-          fi
-        '')
       ];
     };
 
