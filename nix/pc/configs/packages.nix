64dc29ed65eca2cf131b9cef1c89b08c563f0441
Yousef Elbahnasawy <yousef.elbahnasawy@gmail.com>
2026-09-17 12:58:16 +0300

    Removed tmux from zsh config

--- nix/pc/configs/packages.nix
+++ nix/pc/configs/packages.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  config,
   lib,
   inputs,
   ...
@@ -189,12 +188,6 @@
           }
 
           eval "$(atuin init zsh)"
-        '')
-
-        (lib.mkOrder 1100 ''
-          if [[ -z "$TMUX" ]] && command -v tmux >/dev/null 2>&1; then
-            exec tmux new-session -A -s main
-          fi
         '')
       ];
     };
